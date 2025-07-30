d66fc817862faf27f3035f711ef797689f1103d4
ParvathyPadmakumar <puppyparvathy03@gmail.com>
2025-07-30 05:49:10 +0000

    feat: implemented frontend design

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -19,10 +19,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <div className="min-h-screen flex flex-col">
-          <header className="p-4 border-b text-lg font-semibold">System Monitor</header>
+        <div className="min-h-screen flex flex-col ">
+          <header className="p-4 border-b text-teal-50 font-semibold bg-gray-950">System Monitor</header>
           <main className="flex-grow p-6">{children}</main>
-          <footer className="p-4 text-center text-sm opacity-70">© {new Date().getFullYear()} Your Company</footer>
+          <footer className="p-4 text-center text-sm text-white bg-gray-950">© {new Date().getFullYear()} HackSky</footer>
         </div>
       </body>
     </html>
