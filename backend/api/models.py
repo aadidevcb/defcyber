7a347b3008df3d33181b4b0b76f9496325aac297
Aadidev <aadidev1006@gmail.com>
2025-07-31 06:22:27 +0530

    feat: fixes to backend and frontend integration

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,4 +1,15 @@
+
 from django.db import models
+from django.db import models
+
+
+class ApiUser(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128)  # Hashed
+
+    def __str__(self):
+        return self.username
+
 
 class FileHash(models.Model):
     
